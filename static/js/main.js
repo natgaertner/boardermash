3d49754619072472d7bb6fb5c9d6f31409f66c08
Ubuntu <ubuntu@ip-172-31-44-56.us-west-2.compute.internal>
2015-01-20 06:35:33 +0000

    fuck

--- static/js/main.js
+++ static/js/main.js
@@ -1,76 +1,62 @@
+
 $(document).ready(function() {
+	function getboarders() {
+	$.get("/twoboarders", function(data) {
+	    $("#leftboarder").html(data['leftboarder']['boarder_name']);
+	    $("#leftboarder").show();
+	    $("#leftboarder").data("id", data['leftboarder']['boarder_name']);
+	    $("#leftboarder").data("uuid", data['matchuuid']);
+	    if (data['leftboarder']['av']){
+            	$("#leftav").attr("src", "http://duc0plcpp9l5c.cloudfront.net/static/images/avs/"+data['leftboarder']['av']);
+		$("#leftav").show();
+	    } else {
+		$("#leftav").attr("src","");
+		$("#leftav").hide();
+	    }
+	    $("#rightboarder").html(data['rightboarder']['boarder_name']);
+	    $("#rightboarder").show();
+	    $("#rightboarder").data("id", data['rightboarder']['boarder_name']);
+	    if (data['rightboarder']['av']){
+            	$("#rightav").attr("src", "http://duc0plcpp9l5c.cloudfront.net/static/images/avs/"+data['rightboarder']['av']);
+		$("#rightav").show();
+	    } else {
+		$("#rightav").attr("src","");
+		$("#rightav").hide();
+	    }
+	}, "json");
+	};
+	getboarders();
 
-  var ready = false, leftBoarder, rightBoarder,
-    $leftName, $rightName, $leftAv, $rightAv, uuid; 
-  var init = function(){
-    $leftName = $('#leftboarder');
-    $rightName = $('#rightboarder');
-    $leftAv = $('#leftav');
-    $rightAv = $('#rightav');
-  }
-
-  var getBoarders = function() {
-    $.get("/twoboarders", function(data) {
-
-      leftBoarder = data.leftboarder;
-      rightBoarder = data.rightboarder;
-      uuid = data.matchuuid;
-
-      $leftName.text(leftBoarder.boarder_name);
-
-      if (leftBoarder.av) {
-        $leftAv.attr("src", "http://duc0plcpp9l5c.cloudfront.net/static/images/avs/"+data.leftboarder.av);
-        $leftAv.show();
-      } 
-      else {
-        $leftAv.attr("src","");
-      }
-
-      $rightName.text(rightBoarder.boarder_name);
-
-      if (rightBoarder.av) {
-        $rightAv.attr("src", "http://duc0plcpp9l5c.cloudfront.net/static/images/avs/"+data.rightboarder.av);
-        $rightAv.show();
-      } 
-      else {
-        $rightAv.attr("src","");
-      }
-
-      ready = true;
-    }, "json");
-  };
-
-  var mash = function(event) {
-    var winner;
-
-    if (!ready) return;
-
-    if(event.which == 37) {
-      declareWinner("left");
-    } else if (event.which == 39) {
-      declareWinner("right");
-    }
-    
-    getBoarders();
-  };
-
-  var declareWinner = function(winner) {
-    ready = false;
-    $rightAv.hide();
-    $leftAv.hide();
-    $rightName.text('');
-    $leftName.text('');
-    dataToPass = JSON.stringify({leftid:leftBoarder.boarder_name,rightid:rightBoarder.boarder_name,winner:winner,uuid:uuid});
-    $.ajax({
-        type: "POST",
-        url: "/mash",
-        data: dataToPass,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json"
-    });
-  }
-
-  init();
-  getBoarders();
-  $(document).bind('keyup',mash);
+	function debounce(fn, delay) {
+		var timer = null;
+		return function () {
+			var context = this, args = arguments;
+			clearTimeout(timer);
+			timer = setTimeout(function () {
+				fn.apply(context, args);
+			}, delay);
+		};
+	};
+	function mash(event) {
+		var winner;
+		if(event.which == 37) {
+			winner = "left";
+		} else if (event.which == 39) {
+			winner = "right";
+		} else {
+			$(document).bind('keydown',mash);
+			return;
+		}
+		$('.refreshelement').hide();
+		$.ajax({
+		    async: false,
+		    type: "POST",
+		    url: "/mash",
+		    data: JSON.stringify({leftid:$("#leftboarder").data("id"),rightid:$("#rightboarder").data("id"),winner:winner,uuid:$("#leftboarder").data("uuid")}),
+		    contentType: "application/json; charset=utf-8",
+		    dataType: "json"
+		});
+		getboarders();
+	};
+	$(document).bind('keyup',mash);
 });
