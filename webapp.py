9929b0e072b79f047acfe427a34c712e8df2e1b3
Nathaniel Gaertner <natgaertner@gmail.com>
2015-01-21 18:22:39 -0800

    use hmac signature verification instead of storing uuids

--- webapp.py
+++ webapp.py
@@ -2,6 +2,7 @@ from flask import Flask, request, session, render_template
 import json
 import logging
 import redis
+import hmac
 import os
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
@@ -24,7 +25,7 @@ av_r = redis.StrictRedis(host='localhost', port=6379, db=1)
 uuid_r = redis.StrictRedis(host='localhost',port=6379,db=2)
 app.logger.info('connecting to sqs')
 q = boto.sqs.connect_to_region('us-west-2').get_queue(os.getenv("SQS_QUEUE_NAME"))
-app.secret_key = os.getenv('SESSION_SECRET')
+secret_key = os.getenv('SECRET_KEY')
 app.logger.info('setup done')
 
 @app.route('/')
@@ -38,23 +39,18 @@ def twoboarders():
     player1 = ordered_players[idx1]
     player2 = ordered_players[idx2]
     app.logger.debug('leftkey: {player1} rightkey: {player2}'.format(player1=player1.encode('utf-8'), player2=player2.encode('utf-8')))
-    session['rightkey'] = player2
-    session['leftkey'] = player1
     uuid = uuid4().hex
-    uuid_r.setex(uuid,UUID_EX_SECONDS,json.dumps({'leftkey':player1,'rightkey':player2}))
-    session['uuid'] = uuid
-    return json.dumps({'matchuuid':uuid,'leftboarder':{'boarder_name':player1, 'av':av_r.get(player1)},'rightboarder':{'boarder_name':player2, 'av':av_r.get(player2)}})
+    user_data = json.dumps({'uuid':uuid,'leftboarder':player1,'rightboarder':player2})
+    user_data_hmac = hmac.new(secret_key, user_data).hexdigest()
+    return json.dumps({'matchuuid':uuid,'leftboarder':{'boarder_name':player1, 'av':av_r.get(player1)},'rightboarder':{'boarder_name':player2, 'av':av_r.get(player2)},'hmac':user_data_hmac})
 
 @app.route('/mash', methods=['POST'])
 def mash():
     data = dict(request.json)
     data.update({"timestamp":datetime.now().strftime(TIME_FORMAT), "remote_addr":request.headers.get('X-Forwarded-For')})
-    if not data.has_key('uuid'):
-        data['uuid'] = session['uuid']
     try:
         jsondata = json.dumps(data)
         app.logger.debug('enqueuing work {data}'.format(data=jsondata))
-        app.logger.debug('session_key_right: {session_rkey} session_key_left: {session_lkey}'.format(session_rkey=session['rightkey'].encode('utf-8'), session_lkey=session['leftkey'].encode('utf-8')))
         m = Message()
         m.set_body(jsondata)
         q.write(m)
